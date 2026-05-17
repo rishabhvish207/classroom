534f1dc6960e64feec1a1336b328ccff3916bc87
Rishabh <rishabhvish207@gmail.com>
2026-05-17 21:30:30 +0530

    implemet GET all subjects route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,20 @@
 import express from 'express';
+import subjectsRouter from "./routes/subjects";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
+
 app.use(express. json());
+
+app.use('/api/subjects', subjectsRouter);
+
 app.get('/', (req, res) => {
     res.send('Hello, welcome to the Classroom API!');
 });
